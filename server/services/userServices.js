2d505143d28974250ebbcc17036ddf68f4788c33
Matt Rose <mjr68@duke.edu>
2019-11-14 17:53:31 +0100

    added create-react-app components

--- server/services/userServices.js
+++ server/services/userServices.js
@@ -8,7 +8,7 @@ const pool = new Pool({
 })
 
 const getUsers = (request, response) => {
-  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+  pool.query('SELECT * FROM users ORDER BY uid ASC', (error, results) => {
     if (error) {
       throw error
     }
@@ -17,9 +17,9 @@ const getUsers = (request, response) => {
 }
 
 const getUserById = (request, response) => {
-  const id = parseInt(request.params.id)
+  const uid = parseInt(request.params.id)
 
-  pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+  pool.query('SELECT * FROM users WHERE uid = $1', [uid], (error, results) => {
     if (error) {
       throw error
     }
@@ -28,9 +28,9 @@ const getUserById = (request, response) => {
 }
 
 const createUser = (request, response) => {
-  const { name, email } = request.body
+  const { firstname, lastname, email } = request.body
 
-  pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
+  pool.query('INSERT INTO users (firstname, lastname, email) VALUES ($1, $2, $3)', [firstname, lastname, email], (error, results) => {
     if (error) {
       throw error
     }
@@ -39,29 +39,29 @@ const createUser = (request, response) => {
 }
 
 const updateUser = (request, response) => {
-  const id = parseInt(request.params.id)
-  const { name, email } = request.body
+  const uid = parseInt(request.params.id)
+  const { firstname, lastname, email } = request.body
 
   pool.query(
-    'UPDATE users SET name = $1, email = $2 WHERE id = $3',
-    [name, email, id],
+    'UPDATE users SET firstname = $1, lastname = $2, email = $3 WHERE uid = $4',
+    [firstname, lastname, email, uid],
     (error, results) => {
       if (error) {
         throw error
       }
-      response.status(200).send(`User modified with ID: ${id}`)
+      response.status(200).send(`User modified with ID: ${uid}`)
     }
   )
 }
 
 const deleteUser = (request, response) => {
-  const id = parseInt(request.params.id)
+  const uid = parseInt(request.params.id)
 
-  pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+  pool.query('DELETE FROM users WHERE uid = $1', [uid], (error, results) => {
     if (error) {
       throw error
     }
-    response.status(200).send(`User deleted with ID: ${id}`)
+    response.status(200).send(`User deleted with ID: ${uid}`)
   })
 }
 
