09931d5983de9230a27cb88f26cabcacd3f627ae
Matt Rose <mjr68@duke.edu>
2019-11-15 10:50:08 +0100

    incorporated material-ui components

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import SignIn from '../pages/SignIn.js';
+import SignUp from '../pages/SignUp.js';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/register" component={SignUp} />
+      {/*<Route path="/dashboard" component={Dashboard} isPrivate />*/}
+      {/* redirect user to SignIn page if route does not exist and user is not authenticated */}
+      <Route component={SignIn} />
+    </Switch>
+  );
+}
