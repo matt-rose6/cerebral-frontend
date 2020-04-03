f924c7be541c435d5ac964d29117018f0f2a6982
mjr68 <mjr68@duke.edu>
2020-04-03 00:03:20 -0400

    moved backend components to a separate repository

--- server/controllers/index.js
+++
@@ -1,9 +0,0 @@
-const users = require('./userController.js');
-const entries = require('./entryController.js');
-const emotions = require('./emotionController.js');
-
-module.exports = {
-  users,
-  entries,
-  emtions
-};
